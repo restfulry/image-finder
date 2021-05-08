44578a03ae806947578765e585efc729df7b6056
Ryan Lee <ryan@RY-MBP-TB-Local>
2021-05-08 11:19:47 -0400

    style nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
+import "./Nav.css"
+
 const NavBar = () => {
   return (
-    <div className="nav"> 
-      <Link to={{pathname: "/"}} className="nav-link">Home</Link>
+    <div className="nav-container"> 
+      <Link to={{pathname: "/"}} className="nav-link">Image Repo</Link>
       <Link to={{pathname: "/upload"}} className="nav-link">Upload</Link>
       <Link to={{pathname: "/delete"}} className="nav-link">Manage</Link>
     </div>
